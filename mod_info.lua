9ce0fa2e6076a225d0b73ebcd2e5b63072410b10
mirddes <e9fq7jkK!>
2019-11-06 22:25:48 +1300

    initial commit

---
+++ mod_info.lua
@@ -0,0 +1,16 @@
+name = "Infrastructure Pack"
+uid = "fff6f6c8-248a-11de-8c30-362b9e100003"
+version = 3
+copyright = "Ejionar/Varaxis/mythixgirl/Puledro/Yaru/Atom7k/Jagged & some units and textures from Blackops Unleashed & Total Mayhem"
+description = "T2/3 Storage, T4mex, T4 Power, T4 Hydrocarbons. T4 Hives for all - amphibious SMD's - (works with most mods) credit to Ejionar/Varaxis/mythixgirl/Puledro/Yaru/Atom7k/Captain/Chunck/Jagged/Mirddes"
+author = "mirddes"
+icon = "/mods/Infrastructure_Pack/mod_icon.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { }
+requiresNames = { }
+conflicts = { }
+before = { }
+after = { }
