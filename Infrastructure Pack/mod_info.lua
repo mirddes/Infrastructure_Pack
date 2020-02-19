cc5a8231d955ea98e463149713693b3caf266d14
mirddes <mirddes@gmail.com>
2020-02-19 13:07:36 +1300

    Signed-off-by: mirddes <mirddes@gmail.com>

--- Infrastructure Pack/mod_info.lua
+++ Infrastructure Pack/mod_info.lua
@@ -1,9 +1,9 @@
 name = "Infrastructure Pack"
-uid = "fff6f6c8-248a-11de-8c30-362b9e100010"
-version = 10
-copyright = "Ejionar/Varaxis/mythixgirl/Puledro/Yaru/Atom7k/CaptainChunck/Jagged/Uveso/Mirddes & some units and textures from Blackops Unleashed & Total Mayhem"
-description = "T2&T3 Storage. T4mexes. T2,T3,T4 Hydrocarbons. T4 Shields. T4 Hives. T4 Power. credit to Ejionar/Varaxis/mythixgirl/Puledro/Yaru/Atom7k/CaptainChunck/Jagged/Uveso/Mirddes"
-author = "Many Wonderful People"
+uid = "fff6f6c8-248a-11de-8c30-362b9e100011"
+version = 11
+copyright = "Ejionar/Varaxis/mythixgirl/Puledro/Yaru/Atom7k/CaptainChunck/Jagged/Uveso/Mirddes/Quark/DoomHammer/BarryBlack/IceDragon/ExavierMacbeth/Lt_hawkeye/orangeknight/Tpapp/MarLo"
+description = "T2&T3 Storage. T4mexes. T2,T3,T4 Hydrocarbons. T4 AntiNuke Shields. T4 Hives. T4 Power. AntiTeleport. Amphibious AntiNukes. Flying Engineers. credit to Ejionar/Varaxis/mythixgirl/Puledro/Yaru/Atom7k/CaptainChunck/Jagged/Uveso/Mirddes/Quark/DoomHammer/BarryBlack/IceDragon/ExavierMacbeth/Lt_hawkeye/orangeknight/Tpapp/MarLo"
+author = "ManyWonderfulPeople"
 icon = "/mods/Infrastructure Pack/mod_icon.png"
 selectable = true
 enabled = true
