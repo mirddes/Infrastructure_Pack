5f06f035651d04c4c47c027a7cfc56d4e74011eb
Antony <bloodantz14@gmail.com>
2020-02-20 14:38:40 +1300

    fixed copyright symbol

--- Infrastructure Pack/T2_T3_Storage_FAF/units/uab1106/UAB1106_script.lua
+++ Infrastructure Pack/T2_T3_Storage_FAF/units/uab1106/UAB1106_script.lua
@@ -5,7 +5,7 @@
 #**
 #**  Summary  :  Aeon Mass Storage
 #**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#**  Copyright Â© 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
 
 local AMassStorageUnit = import('/lua/aeonunits.lua').AMassStorageUnit
